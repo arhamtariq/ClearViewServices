f2e7d2abc81eee393758bc6c12c5f2e05d3d54df
arham tariq <arhamtariq99@bitbucket.org>
2019-12-16 20:31:08 +0500

    New controller created ,database migrations added for users table,public image folder is added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->integer('user_code');
+            $table->integer('company_code');
+            $table->string('user_name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('phone_number',10);
+            $table->string('address',250);
+            $table->string('city',45);
+            $table->string('state',2);
+            $table->integer('zip_code');
+            $table->string('role',45);
+            $table->string('mother_madain_name',45);  
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
