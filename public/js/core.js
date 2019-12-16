c4236e25d3d283ee6b09b293fd158ca68e22d0d1
Noshaba Mariam <58042781+noshabamariam@users.noreply.github.com>
2019-12-16 19:19:06 +0500

    Add files via upload

---
+++ public/js/core.js
@@ -0,0 +1,87 @@
+$(function() {
+    /*-------------------register form styling-----------------*/
+    $("#overall-form").steps({
+        headerTag: "h2",
+        bodyTag: "section",
+        transitionEffect: "fade",
+        enableAllSteps: true,
+        stepsOrientation: "vertical",
+        autoFocus: true,
+        transitionEffectSpeed: 500,
+        titleTemplate: '<div class="title">#title#</div>',
+        labels: {
+            previous: 'Back Step',
+            next: '<i class = "fa fa-arrow-right" style="color:white;"></i>',
+            finish: '<i class = "fa fa-arrow-right" style="color:white;"> </i>',
+            current: ''
+        },
+        onFinished: function(event, currentIndex) {
+
+            $("#rform").submit();
+
+        }
+    })
+
+    //"use strict";
+    $('.form-text').each(function() {
+        alert('hhh');
+        $(this).on('blur', function() {
+            if ($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            } else {
+                $(this).removeClass('has-val');
+            }
+        })
+    })
+
+    /*-------------------login form validations----------------*/
+    var input = $('.validate-input .form-text');
+
+    $('.validate-form').on('submit', function() {
+        var check = true;
+
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .form-text').each(function() {
+
+        $(this).focus(function() {
+            hideValidate(this);
+        });
+    });
+
+    function validate(input) {
+
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        } else {
+            if ($(input).val().trim() == '') {
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+
+});
